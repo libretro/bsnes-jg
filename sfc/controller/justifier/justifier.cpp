e69ea7c5cbef40c2bcacbe225cfc3855f87f8f2e
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2021-12-14 13:49:54 -0500

    Remove unneeded code from justifier.cpp/hpp

--- sfc/controller/justifier/justifier.cpp
+++ sfc/controller/justifier/justifier.cpp
@@ -91,72 +91,15 @@ auto Justifier::latch(bool data) -> void {
 
 auto Justifier::latch() -> void {
   if(!active) {
-    /*int nx = platform->inputPoll(port, device, 0 + X);
-    int ny = platform->inputPoll(port, device, 0 + Y);
-    player1.x = max(-16, min(256 + 16, nx + player1.x));
-    player1.y = max(-16, min((int)ppu.vdisp() + 16, ny + player1.y));*/
     player1.x = platform->inputPoll(port, device, 0 + X);
     player1.y = platform->inputPoll(port, device, 0 + Y);
     bool offscreen = (player1.x < 0 || player1.y < 0 || player1.x >= 256 || player1.y >= (int)ppu.vdisp());
     if(!offscreen) ppu.latchCounters(player1.x, player1.y);
   }
   else {
-    /*int nx = platform->inputPoll(port, device, 4 + X);
-    int ny = platform->inputPoll(port, device, 4 + Y);
-    player2.x = max(-16, min(256 + 16, nx + player2.x));
-    player2.y = max(-16, min((int)ppu.vdisp() + 16, ny + player2.y));*/
     player2.x = platform->inputPoll(port, device, 4 + X);
     player2.y = platform->inputPoll(port, device, 4 + Y);
     bool offscreen = (player2.x < 0 || player2.y < 0 || player2.x >= 256 || player2.y >= (int)ppu.vdisp());
     if(!offscreen) ppu.latchCounters(player2.x, player2.y);
   }
-}
-
-auto Justifier::draw(uint16_t* data, uint pitch, uint width, uint height) -> void {
-  /*pitch >>= 1;
-  float scaleX = (float)width  / 256.0;
-  float scaleY = (float)height / (float)ppu.vdisp();
-  int length = (float)width / 256.0 * 4.0;
-
-  auto plot = [&](int x, int y, uint16_t color) -> void {
-    if(x >= 0 && y >= 0 && x < (int)width && y < (int)height) {
-      data[y * pitch + x] = color;
-    }
-  };
-
-  { int x = player1.x * scaleX;
-    int y = player1.y * scaleY;
-
-    uint16_t color = 0x03e0;
-    uint16_t black = 0x0000;
-
-    for(int px = x - length - 1; px <= x + length + 1; px++) plot(px, y - 1, black);
-    for(int px = x - length - 1; px <= x + length + 1; px++) plot(px, y + 1, black);
-    for(int py = y - length - 1; py <= y + length + 1; py++) plot(x - 1, py, black);
-    for(int py = y - length - 1; py <= y + length + 1; py++) plot(x + 1, py, black);
-    plot(x - length - 1, y, black);
-    plot(x + length + 1, y, black);
-    plot(x, y - length - 1, black);
-    plot(x, y + length + 1, black);
-    for(int px = x - length; px <= x + length; px++) plot(px, y, color);
-    for(int py = y - length; py <= y + length; py++) plot(x, py, color);
-  }
-
-  if(chained)
-  { int x = player2.x * scaleX;
-    int y = player2.y * scaleY;
-    uint16_t color = 0x7c00;
-    uint16_t black = 0x0000;
-
-    for(int px = x - length - 1; px <= x + length + 1; px++) plot(px, y - 1, black);
-    for(int px = x - length - 1; px <= x + length + 1; px++) plot(px, y + 1, black);
-    for(int py = y - length - 1; py <= y + length + 1; py++) plot(x - 1, py, black);
-    for(int py = y - length - 1; py <= y + length + 1; py++) plot(x + 1, py, black);
-    plot(x - length - 1, y, black);
-    plot(x + length + 1, y, black);
-    plot(x, y - length - 1, black);
-    plot(x, y + length + 1, black);
-    for(int px = x - length; px <= x + length; px++) plot(px, y, color);
-    for(int py = y - length; py <= y + length; py++) plot(x, py, color);
-  }*/
 }
--- sfc/controller/justifier/justifier.hpp
+++ sfc/controller/justifier/justifier.hpp
@@ -8,7 +8,6 @@ struct Justifier : Controller {
   auto data() -> uint2;
   auto latch(bool data) -> void;
   auto latch() -> void override;
-  auto draw(uint16_t* data, uint pitch, uint width, uint height) -> void override;
 
 //private:
   const bool chained;  //true if the second justifier is attached to the first
