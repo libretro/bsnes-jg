fcb16a5d5dcf11989b58f1cd8384bfd9bd9168e3
orbea <orbea@riseup.net>
2021-12-15 22:09:07 -0800

    nall: Remove unused code from arithmetic.hpp

--- nall/arithmetic.hpp
+++ nall/arithmetic.hpp
@@ -4,12 +4,6 @@
 //warning: each size is quadratically more expensive than the size before it!
 
 namespace nall {
-
-  template<typename T, std::enable_if_t<std::is_unsigned<T>::value>>
-  inline auto upper(T value) -> T {
-    return value >> sizeof(T) * 4;
-  }
-
   template<typename T, std::enable_if_t<std::is_unsigned<T>::value>>
   inline auto lower(T value) -> T {
     static const T Mask = ~T(0) >> sizeof(T) * 4;
@@ -27,18 +21,7 @@ namespace nall {
   }
 
   template<typename T, typename U>
-  inline auto rol(T lhs, U rhs, std::enable_if_t<std::is_unsigned<T>::value>* = 0) -> T {
-    return lhs << rhs | lhs >> sizeof(T) * 8 - rhs;
-  }
-
-  template<typename T, typename U>
   inline auto ror(T lhs, U rhs, std::enable_if_t<std::is_unsigned<T>::value>* = 0) -> T {
     return lhs >> rhs | lhs << sizeof(T) * 8 - rhs;
   }
-
-  template<uint Bits> struct ArithmeticNatural;
-  template<> struct ArithmeticNatural<  8> { using type =   uint8_t; };
-  template<> struct ArithmeticNatural< 16> { using type =  uint16_t; };
-  template<> struct ArithmeticNatural< 32> { using type =  uint32_t; };
-  template<> struct ArithmeticNatural< 64> { using type =  uint64_t; };
 }
