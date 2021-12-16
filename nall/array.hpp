1041d2e858fdc8d97a55f9d558790c7dbd906d05
orbea <orbea@riseup.net>
2021-12-15 17:48:46 -0800

    nall: Remove unused code from file-buffer.hpp

--- nall/array.hpp
+++ nall/array.hpp
@@ -76,58 +76,10 @@ template<typename T> struct array_view {
     return {_data + offset, length};
   }
 
-  //array_view<uint8_t> specializations
-  template<typename U> auto readl(U& value, uint size) -> U;
-  template<typename U> auto readm(U& value, uint size) -> U;
-  template<typename U> auto readvn(U& value, uint size) -> U;
-  template<typename U> auto readvi(U& value, uint size) -> U;
-
-  template<typename U> auto readl(U& value, uint offset, uint size) -> U { return view(offset, size).readl(value, size); }
-
-  template<typename U = uint64_t> auto readl(uint size) -> U { U value; return readl(value, size); }
-  template<typename U = uint64_t> auto readm(uint size) -> U { U value; return readm(value, size); }
-  template<typename U = uint64_t> auto readvn(uint size) -> U { U value; return readvn(value, size); }
-  template<typename U =  int64_t> auto readvi(uint size) -> U { U value; return readvi(value, size); }
-
-  template<typename U = uint64_t> auto readl(uint offset, uint size) -> U { U value; return readl(value, offset, size); }
-
 protected:
   const T* _data;
   int _size;
 };
-
-template<> template<typename U> inline auto array_view<uint8_t>::readl(U& value, uint size) -> U {
-  value = 0;
-  for(uint byte : range(size)) value |= (U)read() << byte * 8;
-  return value;
-}
-
-template<> template<typename U> inline auto array_view<uint8_t>::readm(U& value, uint size) -> U {
-  value = 0;
-  for(uint byte : reverse(range(size))) value |= (U)read() << byte * 8;
-  return value;
-}
-
-template<> template<typename U> inline auto array_view<uint8_t>::readvn(U& value, uint size) -> U {
-  value = 0;
-  uint shift = 1;
-  while(true) {
-    auto byte = read();
-    value += (byte & 0x7f) * shift;
-    if(byte & 0x80) break;
-    shift <<= 7;
-    value += shift;
-  }
-  return value;
-}
-
-template<> template<typename U> inline auto array_view<uint8_t>::readvi(U& value, uint size) -> U {
-  value = readvn<U>();
-  bool negate = value & 1;
-  value >>= 1;
-  if(negate) value = ~value;
-  return value;
-}
 
 template<typename T> struct array_span : array_view<T> {
   using type = array_span;
@@ -176,7 +128,6 @@ template<typename T> struct array_span : array_view<T> {
 
   //array_span<uint8_t> specializations
   template<typename U> auto writel(U value, uint size) -> void;
-  template<typename U> auto writem(U value, uint size) -> void;
   template<typename U> auto writevn(U value, uint size) -> void;
   template<typename U> auto writevi(U value, uint size) -> void;
 };
@@ -189,10 +140,6 @@ template<> inline auto array_span<uint8_t>::write(uint8_t value) -> void {
 
 template<> template<typename U> inline auto array_span<uint8_t>::writel(U value, uint size) -> void {
   for(uint byte : range(size)) write(value >> byte * 8);
-}
-
-template<> template<typename U> inline auto array_span<uint8_t>::writem(U value, uint size) -> void {
-  for(uint byte : reverse(range(size))) write(value >> byte * 8);
 }
 
 template<> template<typename U> inline auto array_span<uint8_t>::writevn(U value, uint size) -> void {
