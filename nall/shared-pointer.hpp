ddb1564defc8598fed673029dfe1966b66c3a1c8
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2021-12-16 11:18:08 -0500

    Revert "nall: Remove unneeded code from shared-pointer.hpp"
    This reverts commit 0736ad0dcf88d6098c940a2a8f4fdf89ca68b015.

--- nall/shared-pointer.hpp
+++ nall/shared-pointer.hpp
@@ -15,6 +15,9 @@ struct shared_pointer_manager {
 };
 
 template<typename T> struct shared_pointer;
+template<typename T> struct shared_pointer_weak;
+template<typename T> struct shared_pointer_this;
+struct shared_pointer_this_base{};
 
 template<typename T>
 struct shared_pointer {
@@ -37,17 +40,83 @@ struct shared_pointer {
     operator=(source);
   }
 
+  shared_pointer(T* source, const function<void (T*)>& deleter) {
+    operator=(source);
+    manager->deleter = function<void (void*)>([=](void* p) {
+      deleter((T*)p);
+    });
+  }
+
+  shared_pointer(const shared_pointer& source) {
+    operator=(source);
+  }
+
+  shared_pointer(shared_pointer&& source) {
+    operator=(std::move(source));
+  }
+
   template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
   shared_pointer(const shared_pointer<U>& source) {
     operator=<U>(source);
+  }
+
+  template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
+  shared_pointer(shared_pointer<U>&& source) {
+    operator=<U>(std::move(source));
+  }
+
+  template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
+  shared_pointer(const shared_pointer_weak<U>& source) {
+    operator=<U>(source);
+  }
+
+  template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
+  shared_pointer(const shared_pointer<U>& source, T* pointer) {
+    if((bool)source && (T*)source.manager->pointer == pointer) {
+      manager = source.manager;
+      manager->strong++;
+    }
   }
 
   ~shared_pointer() {
     reset();
   }
 
+  auto operator=(T* source) -> shared_pointer& {
+    reset();
+    if(source) {
+      manager = new shared_pointer_manager((void*)source);
+      manager->strong++;
+      if constexpr(std::is_base_of_v<shared_pointer_this_base, T>) {
+        source->weak = *this;
+      }
+    }
+    return *this;
+  }
+
   auto operator=(const shared_pointer& source) -> shared_pointer& {
     if(this != &source) {
+      reset();
+      if((bool)source) {
+        manager = source.manager;
+        manager->strong++;
+      }
+    }
+    return *this;
+  }
+
+  auto operator=(shared_pointer&& source) -> shared_pointer& {
+    if(this != &source) {
+      reset();
+      manager = source.manager;
+      source.manager = nullptr;
+    }
+    return *this;
+  }
+
+  template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
+  auto operator=(const shared_pointer<U>& source) -> shared_pointer& {
+    if((uintptr_t)this != (uintptr_t)&source) {
       reset();
       if((bool)source) {
         manager = source.manager;
@@ -63,6 +132,16 @@ struct shared_pointer {
       reset();
       manager = source.manager;
       source.manager = nullptr;
+    }
+    return *this;
+  }
+
+  template<typename U, typename = std::enable_if_t<is_compatible<U>::value>>
+  auto operator=(const shared_pointer_weak<U>& source) -> shared_pointer& {
+    reset();
+    if((bool)source) {
+      manager = source.manager;
+      manager->strong++;
     }
     return *this;
   }
@@ -89,6 +168,11 @@ struct shared_pointer {
   template<typename U>
   auto operator==(const shared_pointer<U>& source) const -> bool {
     return manager == source.manager;
+  }
+
+  template<typename U>
+  auto operator!=(const shared_pointer<U>& source) const -> bool {
+    return manager != source.manager;
   }
 
   explicit operator bool() const {
@@ -123,6 +207,80 @@ struct shared_pointer {
     }
     manager = nullptr;
   }
+
+  template<typename U>
+  auto cast() -> shared_pointer<U> {
+    if(auto pointer = dynamic_cast<U*>(data())) {
+      return {*this, pointer};
+    }
+    return {};
+  }
+};
+
+template<typename T>
+struct shared_pointer_weak {
+  using type = T;
+  shared_pointer_manager* manager = nullptr;
+
+  shared_pointer_weak() {
+  }
+
+  shared_pointer_weak(const shared_pointer<T>& source) {
+    operator=(source);
+  }
+
+  auto operator=(const shared_pointer<T>& source) -> shared_pointer_weak& {
+    reset();
+    if(manager = source.manager) manager->weak++;
+    return *this;
+  }
+
+  ~shared_pointer_weak() {
+    reset();
+  }
+
+  auto operator==(const shared_pointer_weak& source) const -> bool {
+    return manager == source.manager;
+  }
+
+  auto operator!=(const shared_pointer_weak& source) const -> bool {
+    return manager != source.manager;
+  }
+
+  explicit operator bool() const {
+    return manager && manager->strong;
+  }
+
+  auto acquire() const -> shared_pointer<T> {
+    return shared_pointer<T>(*this);
+  }
+
+  auto reset() -> void {
+    if(manager && --manager->weak == 0) {
+      if(manager->strong == 0) {
+        delete manager;
+      }
+    }
+    manager = nullptr;
+  }
+};
+
+template<typename T>
+struct shared_pointer_this : shared_pointer_this_base {
+  shared_pointer_weak<T> weak;
+  auto shared() -> shared_pointer<T> { return weak; }
+  auto shared() const -> shared_pointer<T const> { return weak; }
+};
+
+template<typename T, typename... P>
+auto shared_pointer_make(P&&... p) -> shared_pointer<T> {
+  return shared_pointer<T>{new T{std::forward<P>(p)...}};
+}
+
+template<typename T>
+struct shared_pointer_new : shared_pointer<T> {
+  shared_pointer_new(const shared_pointer<T>& source) : shared_pointer<T>(source) {}
+  template<typename... P> shared_pointer_new(P&&... p) : shared_pointer<T>(new T(std::forward<P>(p)...)) {}
 };
 
 }
