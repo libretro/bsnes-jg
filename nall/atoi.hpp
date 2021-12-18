692967989ed18472525a3ca9080e0bc3b9a13e5d
orbea <orbea@riseup.net>
2021-12-17 16:33:02 -0800

    nall: Remove unused code take two
    Reverts 693ba534eb1ed8a04fb90f1c783d4ef64d2a4598
    
    Preserves file-buffer.hpp for now which caused issues.

--- nall/atoi.hpp
+++ nall/atoi.hpp
@@ -38,30 +38,12 @@ constexpr inline auto toHex_(const char* s, uintmax sum = 0) -> uintmax {
   );
 }
 
-//
-
-constexpr inline auto toBinary(const char* s) -> uintmax {
-  return (
-    *s == '0' && (*(s + 1) == 'B' || *(s + 1) == 'b') ? toBinary_(s + 2) :
-    *s == '%' ? toBinary_(s + 1) : toBinary_(s)
-  );
-}
-
-constexpr inline auto toOctal(const char* s) -> uintmax {
-  return (
-    *s == '0' && (*(s + 1) == 'O' || *(s + 1) == 'o') ? toOctal_(s + 2) :
-    toOctal_(s)
-  );
-}
-
 constexpr inline auto toHex(const char* s) -> uintmax {
   return (
     *s == '0' && (*(s + 1) == 'X' || *(s + 1) == 'x') ? toHex_(s + 2) :
     *s == '$' ? toHex_(s + 1) : toHex_(s)
   );
 }
-
-//
 
 constexpr inline auto toNatural(const char* s) -> uintmax {
   return (
@@ -77,8 +59,6 @@ constexpr inline auto toInteger(const char* s) -> intmax {
     *s == '+' ? +toNatural(s + 1) : *s == '-' ? -toNatural(s + 1) : toNatural(s)
   );
 }
-
-//
 
 inline auto toReal(const char* s) -> double {
   return atof(s);
