98fabaf0001bbb9e1096895d2e7abd2d8e111624
orbea <orbea@riseup.net>
2021-12-25 21:05:56 -0800

    dsp4: Remove redefined types

--- src/coprocessor/dsp4.cpp
+++ src/coprocessor/dsp4.cpp
@@ -3,36 +3,22 @@
 namespace SuperFamicom {
 
 namespace DSP4i {
-  #define  bool8  uint8_t
-  #define   int8   int8_t
-  #define  int16  int16_t
-  #define  int32  int32_t
-  #define  int64  int64_t
-  #define  uint8_t  uint8_t
-  #define uint16 uint16_t
-  #define uint32 uint32_t
-  #define uint64 uint64_t
   #define DSP4_CPP
-  inline uint16 READ_WORD(uint8_t *addr) {
+
+  inline uint16_t READ_WORD(uint8_t *addr) {
     return (addr[0]) + (addr[1] << 8);
   }
-  inline uint32 READ_DWORD(uint8_t *addr) {
+
+  inline uint32_t READ_DWORD(uint8_t *addr) {
     return (addr[0]) + (addr[1] << 8) + (addr[2] << 16) + (addr[3] << 24);
   }
-  inline void WRITE_WORD(uint8_t *addr, uint16 data) {
+
+  inline void WRITE_WORD(uint8_t *addr, uint16_t data) {
     addr[0] = data;
     addr[1] = data >> 8;
   }
+
   #include "dsp4/dsp4emu.c"
-  #undef  bool8
-  #undef   int8
-  #undef  int16
-  #undef  int32
-  #undef  int64
-  #undef  uint8
-  #undef uint16
-  #undef uint32
-  #undef uint64
 }
 
 DSP4 dsp4;
