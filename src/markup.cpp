e4a490b92518f8e7bd2eb53617c21e8ed1a2e2c2
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2022-01-01 15:46:58 -0500

    markup: Remove unused code

--- src/markup.cpp
+++ src/markup.cpp
@@ -2,52 +2,9 @@
 #include <sstream>
 
 #include "emulator.hpp"
+#include "markup.hpp"
 
 namespace BML {
-
-Markup::Node unserialize(const nall::string& markup, nall::string_view spacing) {
-  nall::shared_pointer<ManagedNode> node(new ManagedNode);
-  try {
-    node->parse(markup, spacing);
-  } catch(const char* error) {
-    node.reset();
-  }
-  return (Markup::SharedNode&)node;
-}
-
-nall::string serialize(const Markup::Node& node, nall::string_view spacing, unsigned depth) {
-  if(!node.name()) {
-    nall::string result;
-    for(auto leaf : node) {
-      result.append(serialize(leaf, spacing, depth));
-    }
-    return result;
-  }
-
-  nall::string padding;
-  padding.resize(depth * 2);
-  padding.fill(' ');
-
-  nall::vector<nall::string> lines;
-  if(auto value = node.value()) lines = value.split("\n");
-
-  nall::string result;
-  result.append(padding);
-  result.append(node.name());
-  if(lines.size() == 1) result.append(":", spacing, lines[0]);
-  result.append("\n");
-  if(lines.size() > 1) {
-    padding.append("  ");
-    for(auto& line : lines) {
-      result.append(padding, ":", spacing, line, "\n");
-    }
-  }
-  for(auto leaf : node) {
-    result.append(serialize(leaf, spacing, depth + 1));
-  }
-  return result;
-}
-
 
 class streamreader : public byuuML::reader {
     char buf[512];
