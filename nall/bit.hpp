e21d265db494d6438c9978b970ab848d0697d113
orbea <orbea@riseup.net>
2022-01-03 09:59:20 -0800

    nall: Remove unused code from bit.hpp

--- nall/bit.hpp
+++ nall/bit.hpp
@@ -2,13 +2,8 @@
 
 //note: (u)intmax actually mean it: use as many bits as is possible
 #if defined(__SIZEOF_INT128__)
-  using int128_t = signed __int128;
-  using uint128_t = unsigned __int128;
-
-  using intmax = int128_t;
-  using uintmax = uint128_t;
+  using uintmax = unsigned __int128;
 #else
-  using intmax = intmax_t;
   using uintmax = uintmax_t;
 #endif
 
