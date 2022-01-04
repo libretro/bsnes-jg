d931159249039ea0f67af625f432ee535d9b7f21
orbea <orbea@riseup.net>
2022-01-03 18:29:27 -0800

    nall: Remove unused code from nall::memory

--- nall/memory.hpp
+++ nall/memory.hpp
@@ -21,19 +21,6 @@
 #pragma once
 
 namespace nall::memory {
-  template<typename T = uint8_t> inline auto allocate(unsigned size) -> T*;
-  template<typename T = uint8_t> inline auto allocate(unsigned size, const T& value) -> T*;
-
-  template<typename T = uint8_t> inline auto resize(void* target, unsigned size) -> T*;
-
-  inline auto free(void* target) -> void;
-
-  template<typename T = uint8_t> inline auto compare(const void* target, unsigned capacity, const void* source, unsigned size) -> int;
-  template<typename T = uint8_t> inline auto compare(const void* target, const void* source, unsigned size) -> int;
-
-  template<typename T = uint8_t> inline auto icompare(const void* target, unsigned capacity, const void* source, unsigned size) -> int;
-  template<typename T = uint8_t> inline auto icompare(const void* target, const void* source, unsigned size) -> int;
-
   template<typename T = uint8_t> inline auto move(void* target, unsigned capacity, const void* source, unsigned size) -> T*;
   template<typename T = uint8_t> inline auto move(void* target, const void* source, unsigned size) -> T*;
 
@@ -49,59 +36,6 @@ namespace nall::memory {
 //memcmp, memcpy, memmove have terrible performance on small block sizes (FreeBSD 10.0-amd64)
 //as this library is used extensively by nall/string, and most strings tend to be small,
 //this library hand-codes these functions instead. surprisingly, it's a substantial speedup
-
-template<typename T> auto allocate(unsigned size) -> T* {
-  return (T*)malloc(size * sizeof(T));
-}
-
-template<typename T> auto allocate(unsigned size, const T& value) -> T* {
-  auto result = allocate<T>(size);
-  if(result) fill<T>(result, size, value);
-  return result;
-}
-
-template<typename T> auto resize(void* target, unsigned size) -> T* {
-  return (T*)realloc(target, size * sizeof(T));
-}
-
-auto free(void* target) -> void {
-  ::free(target);
-}
-
-template<typename T> auto compare(const void* target, unsigned capacity, const void* source, unsigned size) -> int {
-  auto t = (uint8_t*)target;
-  auto s = (uint8_t*)source;
-  auto l = std::min(capacity, size) * sizeof(T);
-  while(l--) {
-    auto x = *t++;
-    auto y = *s++;
-    if(x != y) return x - y;
-  }
-  if(capacity == size) return 0;
-  return -(capacity < size);
-}
-
-template<typename T> auto compare(const void* target, const void* source, unsigned size) -> int {
-  return compare<T>(target, size, source, size);
-}
-
-template<typename T> auto icompare(const void* target, unsigned capacity, const void* source, unsigned size) -> int {
-  auto t = (uint8_t*)target;
-  auto s = (uint8_t*)source;
-  auto l = std::min(capacity, size) * sizeof(T);
-  while(l--) {
-    auto x = *t++;
-    auto y = *s++;
-    if(x - 'A' < 26) x += 32;
-    if(y - 'A' < 26) y += 32;
-    if(x != y) return x - y;
-  }
-  return -(capacity < size);
-}
-
-template<typename T> auto icompare(const void* target, const void* source, unsigned size) -> int {
-  return icompare<T>(target, size, source, size);
-}
 
 template<typename T> auto move(void* target, unsigned capacity, const void* source, unsigned size) -> T* {
   auto t = (uint8_t*)target;
