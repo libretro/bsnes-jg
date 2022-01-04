2b25439b27f29fbe69d28c2fbf8e79066d66ff43
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2022-01-04 14:21:08 -0500

    nall: Remove nall::Boolean from primitives.hpp

--- nall/primitives.hpp
+++ nall/primitives.hpp
@@ -154,22 +154,6 @@ private:
   unsigned shift;
 };
 
-struct Boolean {
-  inline Boolean() : data(false) {}
-  template<typename T> inline Boolean(const T& value) : data(value) {}
-
-  inline operator bool() const { return data; }
-
-  inline auto lower() { return data == 1 ? data = 0, true : false; }
-
-  inline auto flip(bool value) { return data != value ? (data = value, true) : false; }
-  inline auto raise(bool value) { return !data && value ? (data = value, true) : (data = value, false); }
-  inline auto lower(bool value) { return data && !value ? (data = value, true) : (data = value, false); }
-
-private:
-  bool data;
-};
-
 template<unsigned Precision> struct Natural {
   static inline constexpr auto bits() -> unsigned { return Precision; }
   using utype =
