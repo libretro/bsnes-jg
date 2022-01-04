1fa95eed8bd14802d9b354aed1908052656f88c6
orbea <orbea@riseup.net>
2022-01-03 18:51:09 -0800

    nall: Remove unused code from iterator.hpp

--- nall/iterator.hpp
+++ nall/iterator.hpp
@@ -22,33 +22,6 @@
 
 namespace nall {
 
-template<typename T> struct iterator {
-  iterator(T* self, uint64_t offset) : _self(self), _offset(offset) {}
-  auto operator*() -> T& { return _self[_offset]; }
-  auto operator!=(const iterator& source) const -> bool { return _offset != source._offset; }
-  auto operator++() -> iterator& { return _offset++, *this; }
-  auto offset() const -> uint64_t { return _offset; }
-
-private:
-  T* _self;
-  uint64_t _offset;
-};
-
-template<typename T> struct iterator_const {
-  iterator_const(const T* self, uint64_t offset) : _self(self), _offset(offset) {}
-  auto operator*() -> const T& { return _self[_offset]; }
-  auto operator!=(const iterator_const& source) const -> bool { return _offset != source._offset; }
-  auto operator++() -> iterator_const& { return _offset++, *this; }
-  auto offset() const -> uint64_t { return _offset; }
-
-private:
-  const T* _self;
-  uint64_t _offset;
-};
-
-template<typename T> auto rbegin(T& self) { return self.rbegin(); }
-template<typename T> auto rend(T& self) { return self.rend(); }
-
 struct range_t {
   struct iterator {
     iterator(int64_t position, int64_t step = 0) : position(position), step(step) {}
