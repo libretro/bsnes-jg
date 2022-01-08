5df87140fc18f4843cf971ed5f1d317bf77cf584
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2022-01-07 21:23:56 -0500

    gsu: Remove nall::Natural

--- src/processor/gsu.cpp
+++ src/processor/gsu.cpp
@@ -36,34 +36,34 @@ void GSU::instruction(uint8_t opcode) {
     case id: return instruction##name(__VA_ARGS__); \
 
   #define op4(id, name) \
-    case id+ 0: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 1: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 2: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 3: return instruction##name((nall::Natural< 4>)opcode); \
+    case id+ 0: return instruction##name(opcode & 0x0f); \
+    case id+ 1: return instruction##name(opcode & 0x0f); \
+    case id+ 2: return instruction##name(opcode & 0x0f); \
+    case id+ 3: return instruction##name(opcode & 0x0f); \
 
   #define op6(id, name) \
     op4(id, name) \
-    case id+ 4: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 5: return instruction##name((nall::Natural< 4>)opcode); \
+    case id+ 4: return instruction##name(opcode & 0x0f); \
+    case id+ 5: return instruction##name(opcode & 0x0f); \
 
   #define op12(id, name) \
     op6(id, name) \
-    case id+ 6: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 7: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 8: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+ 9: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+10: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+11: return instruction##name((nall::Natural< 4>)opcode); \
+    case id+ 6: return instruction##name(opcode & 0x0f); \
+    case id+ 7: return instruction##name(opcode & 0x0f); \
+    case id+ 8: return instruction##name(opcode & 0x0f); \
+    case id+ 9: return instruction##name(opcode & 0x0f); \
+    case id+10: return instruction##name(opcode & 0x0f); \
+    case id+11: return instruction##name(opcode & 0x0f); \
 
   #define op15(id, name) \
     op12(id, name) \
-    case id+12: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+13: return instruction##name((nall::Natural< 4>)opcode); \
-    case id+14: return instruction##name((nall::Natural< 4>)opcode); \
+    case id+12: return instruction##name(opcode & 0x0f); \
+    case id+13: return instruction##name(opcode & 0x0f); \
+    case id+14: return instruction##name(opcode & 0x0f); \
 
   #define op16(id, name) \
     op15(id, name) \
-    case id+15: return instruction##name((nall::Natural< 4>)opcode); \
+    case id+15: return instruction##name(opcode & 0x0f); \
 
   switch(opcode) {
   op  (0x00, STOP)
