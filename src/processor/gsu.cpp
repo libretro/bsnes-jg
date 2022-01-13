26464675d60bf29c6846e898c9f9fc0f2905dd71
orbea <orbea@riseup.net>
2022-01-13 11:42:56 -0800

    gsu: Remove the op macro
    Also silences more -Wgnu-zero-variadic-macro-arguments warnings.

--- src/processor/gsu.cpp
+++ src/processor/gsu.cpp
@@ -32,9 +32,6 @@
 namespace Processor {
 
 void GSU::instruction(uint8_t opcode) {
-  #define op(id, name, ...) \
-    case id: return instruction##name(__VA_ARGS__); \
-
   #define op4(id, name) \
     case id+ 0: return instruction##name(opcode & 0x0f); \
     case id+ 1: return instruction##name(opcode & 0x0f); \
@@ -66,59 +63,58 @@ void GSU::instruction(uint8_t opcode) {
     case id+15: return instruction##name(opcode & 0x0f); \
 
   switch(opcode) {
-    op  (0x00, STOP)
-    op  (0x01, NOP)
-    op  (0x02, CACHE)
-    op  (0x03, LSR)
-    op  (0x04, ROL)
-    op  (0x05, Branch, 1)  //bra
-    op  (0x06, Branch, (regs.sfr.flag.s ^ regs.sfr.flag.ov) == 0)  //blt
-    op  (0x07, Branch, (regs.sfr.flag.s ^ regs.sfr.flag.ov) == 1)  //bge
-    op  (0x08, Branch, regs.sfr.flag.z == 0)  //bne
-    op  (0x09, Branch, regs.sfr.flag.z == 1)  //beq
-    op  (0x0a, Branch, regs.sfr.flag.s == 0)  //bpl
-    op  (0x0b, Branch, regs.sfr.flag.s == 1)  //bmi
-    op  (0x0c, Branch, regs.sfr.flag.cy == 0)  //bcc
-    op  (0x0d, Branch, regs.sfr.flag.cy == 1)  //bcs
-    op  (0x0e, Branch, regs.sfr.flag.ov == 0)  //bvc
-    op  (0x0f, Branch, regs.sfr.flag.ov == 1)  //bvs
+    case 0x00: return instructionSTOP();
+    case 0x01: return instructionNOP();
+    case 0x02: return instructionCACHE();
+    case 0x03: return instructionLSR();
+    case 0x04: return instructionROL();
+    case 0x05: return instructionBranch(1);  //bra
+    case 0x06: return instructionBranch((regs.sfr.flag.s ^ regs.sfr.flag.ov) == 0);  //blt
+    case 0x07: return instructionBranch((regs.sfr.flag.s ^ regs.sfr.flag.ov) == 1);  //bge
+    case 0x08: return instructionBranch(regs.sfr.flag.z == 0);  //bne
+    case 0x09: return instructionBranch(regs.sfr.flag.z == 1);  //beq
+    case 0x0a: return instructionBranch(regs.sfr.flag.s == 0);  //bpl
+    case 0x0b: return instructionBranch(regs.sfr.flag.s == 1);  //bmi
+    case 0x0c: return instructionBranch(regs.sfr.flag.cy == 0);  //bcc
+    case 0x0d: return instructionBranch(regs.sfr.flag.cy == 1);  //bcs
+    case 0x0e: return instructionBranch(regs.sfr.flag.ov == 0);  //bvc
+    case 0x0f: return instructionBranch(regs.sfr.flag.ov == 1);  //bvs
     op16(0x10, TO_MOVE)
     op16(0x20, WITH)
     op12(0x30, Store)
-    op  (0x3c, LOOP)
-    op  (0x3d, ALT1)
-    op  (0x3e, ALT2)
-    op  (0x3f, ALT3)
+    case 0x3c: return instructionLOOP();
+    case 0x3d: return instructionALT1();
+    case 0x3e: return instructionALT2();
+    case 0x3f: return instructionALT3();
     op12(0x40, Load)
-    op  (0x4c, PLOT_RPIX)
-    op  (0x4d, SWAP)
-    op  (0x4e, COLOR_CMODE)
-    op  (0x4f, NOT)
+    case 0x4c: return instructionPLOT_RPIX();
+    case 0x4d: return instructionSWAP();
+    case 0x4e: return instructionCOLOR_CMODE();
+    case 0x4f: return instructionNOT();
     op16(0x50, ADD_ADC)
     op16(0x60, SUB_SBC_CMP)
-    op  (0x70, MERGE)
+    case 0x70: return instructionMERGE();
     op15(0x71, AND_BIC)
     op16(0x80, MULT_UMULT)
-    op  (0x90, SBK)
+    case 0x90: return instructionSBK();
     op4 (0x91, LINK)
-    op  (0x95, SEX)
-    op  (0x96, ASR_DIV2)
-    op  (0x97, ROR)
+    case 0x95: return instructionSEX();
+    case 0x96: return instructionASR_DIV2();
+    case 0x97: return instructionROR();
     op6 (0x98, JMP_LJMP)
-    op  (0x9e, LOB)
-    op  (0x9f, FMULT_LMULT)
+    case 0x9e: return instructionLOB();
+    case 0x9f: return instructionFMULT_LMULT();
     op16(0xa0, IBT_LMS_SMS)
     op16(0xb0, FROM_MOVES)
-    op  (0xc0, HIB)
+    case 0xc0: return instructionHIB();
     op15(0xc1, OR_XOR)
     op15(0xd0, INC)
-    op  (0xdf, GETC_RAMB_ROMB)
+    case 0xdf: return instructionGETC_RAMB_ROMB();
     op15(0xe0, DEC)
-    op  (0xef, GETB)
+    case 0xef: return instructionGETB();
     op16(0xf0, IWT_LM_SM)
   }
 
-  #undef op
   #undef op4
   #undef op6
   #undef op12
