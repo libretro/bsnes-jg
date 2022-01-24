bfcd6ecfdd52146ef3fe236f4471dde8249fa1fc
orbea <orbea@riseup.net>
2022-01-24 13:41:34 -0800

    dsp: Don't define variables in the header

--- src/dsp.hpp
+++ src/dsp.hpp
@@ -26,8 +26,8 @@ struct DSP {
   uint8_t apuram[64 * 1024] = {};
 
   void main();
-  uint8_t read(uint8_t address);
-  void write(uint8_t address, uint8_t data);
+  uint8_t read(uint8_t);
+  void write(uint8_t, uint8_t);
 
   bool load();
   void power(bool);
