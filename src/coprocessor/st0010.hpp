86bacf091fc1dfc08823bb517f41707c219d84dc
orbea <orbea@riseup.net>
2022-01-24 15:10:14 -0800

    st0010: Move definitions out of the ST0010 struct

--- src/coprocessor/st0010.hpp
+++ src/coprocessor/st0010.hpp
@@ -25,8 +25,8 @@ namespace SuperFamicom {
 struct ST0010 {
   void power();
 
-  uint8_t read(unsigned addr, uint8_t data);
-  void write(unsigned addr, uint8_t data);
+  uint8_t read(unsigned, uint8_t);
+  void write(unsigned, uint8_t);
 
   void serialize(serializer&);
 
@@ -36,16 +36,16 @@ struct ST0010 {
   static const uint8_t arctan[32][32];
 
   //interfaces to sin table
-  int16_t sin(int16_t theta);
-  int16_t cos(int16_t theta);
+  int16_t sin(int16_t);
+  int16_t cos(int16_t);
 
   //interfaces to ram buffer
-  uint8_t readb (uint16_t addr);
-  uint16_t readw (uint16_t addr);
-  uint32_t readd (uint16_t addr);
-  void writeb(uint16_t addr, uint8_t  data);
-  void writew(uint16_t addr, uint16_t data);
-  void writed(uint16_t addr, uint32_t data);
+  uint8_t readb (uint16_t);
+  uint16_t readw (uint16_t);
+  uint32_t readd (uint16_t);
+  void writeb(uint16_t, uint8_t);
+  void writew(uint16_t, uint16_t);
+  void writed(uint16_t, uint32_t);
 
   //opcodes
   void op_01();
@@ -57,7 +57,7 @@ struct ST0010 {
   void op_07();
   void op_08();
 
-  void op_01(int16_t x0, int16_t y0, int16_t &x1, int16_t &y1, int16_t &quadrant, int16_t &theta);
+  void op_01(int16_t, int16_t, int16_t&, int16_t&, int16_t&, int16_t&);
 };
 
 extern ST0010 st0010;
