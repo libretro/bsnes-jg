ead472af207b6008caa645c1c28a9cf9efcf833e
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2023-01-21 13:18:33 -0500

    libco: Remove unnecessary pragma

--- deps/libco/libco.c
+++ deps/libco/libco.c
@@ -1,6 +1,4 @@
 #if defined(__clang__)
-  #pragma clang diagnostic ignored "-Wparentheses"
-
   /* placing code in section(text) does not mark it executable with Clang. */
   #undef  LIBCO_MPROTECT
   #define LIBCO_MPROTECT
