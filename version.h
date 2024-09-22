5376dc9e2154def98a3d1c4b867ccb7c632baaf7
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2024-09-22 12:23:38 -0400

    Version 2.0.0

--- version.h
+++ version.h
@@ -3,11 +3,11 @@
 # this is the only file required to update when changing the version
 
 # https://semver.org/
-VERSION_MAJOR=1
-VERSION_MINOR=2
+VERSION_MAJOR=2
+VERSION_MINOR=0
 VERSION_PATCH=0
 #endif
 
 #ifndef JG_VERSION
-#define JG_VERSION "1.2.0"
+#define JG_VERSION "2.0.0"
 #endif
