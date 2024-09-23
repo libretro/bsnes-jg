32af9e0ac95d4358ea48e7d2428f767ce8ce0607
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2024-09-23 14:52:35 -0400

    libretro: Add buildbot support

---
+++ libretro/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+ROOT_DIR := $(LOCAL_PATH)/../..
+CORE_DIR := $(ROOT_DIR)
+
+include $(ROOT_DIR)/libretro/Makefile.common
+
+COREFLAGS := -DANDROID -D__LIBRETRO__ $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(ROOT_DIR)/libretro/link.T
+include $(BUILD_SHARED_LIBRARY)
